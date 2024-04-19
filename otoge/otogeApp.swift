652af02f3e405550bfce700d6dd4e14723cb749b
Sena Cahya <senacandrampc@gmail.com>
2024-04-19 19:51:39 +0900

    Initial Commit

---
+++ otoge/otogeApp.swift
@@ -0,0 +1,17 @@
+//
+//  otogeApp.swift
+//  otoge
+//
+//  Created by Sen on 2024/4/19.
+//
+
+import SwiftUI
+
+@main
+struct otogeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
